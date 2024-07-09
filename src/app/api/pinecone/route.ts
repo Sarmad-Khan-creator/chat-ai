ab5358d9a96b2a264ca4e4520e9594c9e119f2c8
Sarmad Khan <sarmadkhan2694@gmail.com>
2024-07-09 20:46:43 +0500

    committing changes

--- src/app/api/pinecone/route.ts
+++ src/app/api/pinecone/route.ts
@@ -9,13 +9,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { revalidatePath } from "next/cache";
 
-// export type CreateAppProps = {
-//   title: string;
-//   index: string;
-//   template: string;
-//   blob: string;
-// };
-
 export async function POST(req: NextRequest) {
   const data = await req.formData();
 
@@ -27,9 +20,6 @@ export async function POST(req: NextRequest) {
   if (!title || !index || !template || !file) {
     return NextResponse.json({ success: false });
   }
-
-  // const bytes = await file.arrayBuffer();
-  // const buffer = Buffer.from(bytes);
 
   const clerkUser = await currentUser();
 
